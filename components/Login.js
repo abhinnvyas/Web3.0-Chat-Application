26d4b6559b5cafb01721800105d9b349c5c87ef1
Abhinn Vyas <abhinnvyas2@gmail.com>
2021-12-25 03:47:52 -0800

    Created a beautiful Login button

--- components/Login.js
+++ components/Login.js
@@ -2,11 +2,14 @@ import Image from "next/image";
 
 function Login() {
   return (
-    <div className="bg-black relative text-white">
-      <h1>This is the Login Screen!</h1>
-
+    <div className="bg-black relative text-black">
       <div className="flex absolute z-50 items-center justify-center h-3/4 w-full">
-        <button classname="bg-yellow-400 p-5">Login to the Metaverse!</button>
+        <button
+          className="bg-yellow-400 p-5 rounded-full font-bold shadow-lg hover:scale-105
+        transtion transform duration-200 ease-in-out active:scale-95 hover:shadow-xl active:shadow-2xl"
+        >
+          Login to the Metaverse!
+        </button>
       </div>
 
       <div className="w-full h-screen">
