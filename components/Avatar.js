ebd41118fdf5fbcc869a146e35c6e54489bbc24e
Abhinn Vyas <abhinnvyas2@gmail.com>
2021-12-28 21:49:09 -0800

    Built the Avatar

--- components/Avatar.js
+++ components/Avatar.js
@@ -5,6 +5,8 @@ function Avatar({ username, logOutOnPress }) {
   const { user, logout } = useMoralis();
   return (
     <Image
+      className="rounded-full bg-black cursor-pointer hover:opacity-75
+      transition transform duration-200 ease-in-out"
       src={`https://avatars.dicebear.com/api/pixel-art/${
         username || user.get("username")
       }.svg`}
--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,10 @@ function Header() {
     <div>
       <div className="">
         <div>
-          <div className="">
+          <div
+            className="relative h-48 w-48 lg:mx-auto border-pink-500 rounded-full 
+          border-4 lg:border-8"
+          >
             <Avatar logOutOnPress />
           </div>
           {/* Welcome Message */}
