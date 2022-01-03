70fd52d9a3d4765b367a6899135e38584053df3d
Abhinn Vyas <abhinnvyas2@gmail.com>
2022-01-03 05:10:09 -0800

    Currently Building the Messages Component

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,10 @@ import Head from "next/head";
 import Login from "../components/Login";
 import { useMoralis } from "react-moralis";
 import Header from "../components/Header";
+import Messages from "../components/Messages";
 
 export default function Home() {
-  const { isAuthenticated, logout } = useMoralis();
+  const { isAuthenticated } = useMoralis();
 
   if (!isAuthenticated) return <Login />;
 
@@ -20,7 +21,7 @@ export default function Home() {
 
       <div className="max-w-screen-2xl mx-auto">
         <Header />
-        {/* Messages */}
+        <Messages />
         {/* Footer */}
       </div>
     </div>
