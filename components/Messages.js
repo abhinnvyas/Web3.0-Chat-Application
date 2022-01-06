c24d83e78b55353ecac74b5794b899b83f60c466
Abhinn Vyas <abhinnvyas2@gmail.com>
2022-01-06 07:52:57 -0800

    The get the messages for only the past 15 min feature not working.

--- components/Messages.js
+++ components/Messages.js
@@ -1,10 +1,31 @@
 import { ByMoralis, useMoralis, useMoralisQuery } from "react-moralis";
 import SendMessage from "./SendMessage";
 import { useRef } from "react";
+import Message from "./Message";
+
+// only show messages from the last 15 mins
+const MINS_DURATION = 15;
 
 function Messages() {
   const { user } = useMoralis();
   const endOfMessagesRef = useRef(null);
+  const { data, isLoading, error } = useMoralisQuery(
+    "Messages",
+    (query) => query.ascending("createdAt"),
+    //     .greaterThan(
+    //       "createdAt",
+    //       new Date(Date.now() - 1000 * 60 * MINS_DURATION).toUTCString()
+    //     ),
+    // [MINS_DURATION],
+    [],
+    {
+      live: true, //this makes it a realtime listener
+    }
+  );
+
+  // console.log(new Date(Date.now() - 1000 * 60 * MINS_DURATION));
+  console.log(data);
+  console.log(new Date(Date.now() - 1000 * 60 * MINS_DURATION).toUTCString());
 
   return (
     <div className="pb-56">
@@ -15,7 +36,11 @@ function Messages() {
         />
       </div>
 
-      <div>{/* Each Message */}</div>
+      <div>
+        {data.map((message) => (
+          <Message key={message.id} message={message} />
+        ))}
+      </div>
 
       <div className="flex justify-center">
         <SendMessage endOfMessagesRef={endOfMessagesRef} />
